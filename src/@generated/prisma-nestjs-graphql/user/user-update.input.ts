11509cdf636443e759dc2892d6ce898b6f24b988
r-unit-mano <ms2geki@gmail.com>
2022-05-05 08:52:09 +0900

    init

---
+++ src/@generated/prisma-nestjs-graphql/user/user-update.input.ts
@@ -0,0 +1,18 @@
+import { Field } from '@nestjs/graphql';
+import { InputType } from '@nestjs/graphql';
+import { StringFieldUpdateOperationsInput } from '../prisma/string-field-update-operations.input';
+import { NullableStringFieldUpdateOperationsInput } from '../prisma/nullable-string-field-update-operations.input';
+import { PostUpdateManyWithoutAuthorInput } from '../post/post-update-many-without-author.input';
+
+@InputType()
+export class UserUpdateInput {
+
+    @Field(() => StringFieldUpdateOperationsInput, {nullable:true})
+    email?: StringFieldUpdateOperationsInput;
+
+    @Field(() => NullableStringFieldUpdateOperationsInput, {nullable:true})
+    name?: NullableStringFieldUpdateOperationsInput;
+
+    @Field(() => PostUpdateManyWithoutAuthorInput, {nullable:true})
+    posts?: PostUpdateManyWithoutAuthorInput;
+}
